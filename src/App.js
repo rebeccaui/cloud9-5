628f8d109e7d2b0dac720ca3a7f80612fd871dd2
rwieberdink <rebeccawieberdink@gmail.com>
2019-02-08 10:51:01 -0500

    text input working and alerting result

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import logo from './cloud9-5-logo.png';
 import './App.css';
 
 // import API from "../../utils/API";
-// import axios from 'axios';
+import axios from 'axios';
 
 //Route Dependencies
 //==========================================
@@ -22,52 +22,53 @@ class App extends Component {
     this.state = {
       weatherArray: [],
       zipcode: '',
+      coordinates: {},
       todayCurrentTemp: '',
       todayHigh: '',
       todayLow: '',
-
+      todayPressure: '',
+      todayHumidity: ''
     }
 
-    
-    // this.getUser =
-    //   this.getUser.bind(this);
+    this.handleChange = 
+      this.handleChange.bind(this);
 
-    this.componentDidMount =
-      this.componentDidMount.bind(this);
+    this.handleSubmit = 
+      this.handleSubmit.bind(this);
 
     this.getWeather =
       this.getWeather.bind(this);
-
   }
+
   // App Functions 
   //================================================
 
-  //Once the page loads, call function getWeather
-  componentDidMount() {
-    //this.getUser()
-    this.getWeather()
-  } 
+  // Zipcode form change/submit
+  handleChange(event) {
+    this.setState({zipcode: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A zipcode was submitted: ' + this.state.zipcode);
+    event.preventDefault();
+    this.getWeather();
+  }
 
   //An axios call to retrieve weather information from the OpenWeather API
-  getWeather() {
+  getWeather(zipcode) {
     axios.get('api.openweathermap.org/data/2.5/weather?zip=' + zipcode)
     .then(res => {
       for(let i = 0; i < res.data.length; i++) {
         this.state.weatherArray.push({
-          weatherArray: [],
-          zipcode: '',
-          todayCurrentTemp: '',// res.data[i].location.coordinates[0],
-          todayHigh: '',
-          todayLow: '',
+          coordinates: res.data[i].coord,
+          todayCurrentTemp: res.data[i].main.temp,
+          todayHigh: res.data[i].main.temp_max,
+          todayLow: res.data[i].main.temp_min,
+          todayPressure: res.data[i].main.pressure,
+          todayHumidity: res.data[i].main.humidity
         })
       }
     })
-  }
-
-  //Load the weather object into the page's state
-  loadWeather(weatherObject) {
-    this.setState(weatherObject)
-
   }
 
   render() {
@@ -79,20 +80,37 @@ class App extends Component {
           <img src={logo} className="cloud95-logo" alt="logo" />
           <h1>Cloud 9-5</h1>
           <p><strong>Weather, you like it or not?!</strong></p>
+          <br />
+          <form onSubmit={this.handleSubmit}>
+              <label>
+                Zip Code: 
+                <input type="text" value={this.state.zipcode} onChange={this.handleChange} />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+
           <div className="topBorder"></div>
         </header>
+
+
         <div className="cloud95-today" id="today">
           today
           <div className="bottomBorder"></div>
         </div>
+
+
         <div className="cloud95-forecast">
           forecast
           <div className="topBorder"></div>
         </div>
+
+
         <div className="cloud95-radar">
           radar
           <div className="bottomBorder"></div>
         </div>
+
+
         <div className="cloud95-puns">
           puns
           <div className="topBorder"></div>
